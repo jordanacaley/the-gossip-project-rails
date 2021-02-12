fdc00c4564bd30305e94229910d32304709147f0
Jordan <jordancaley10@gmail.com>
2021-02-12 13:13:11 +0100

    Add 10 random users to database with Faker

--- Gemfile
+++ Gemfile
@@ -54,3 +54,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# Faker to generate random users
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+
+# Display tables
+gem 'table_print'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+10.times do
+  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Hipster.sentence(word_count: 10), email: Faker::Internet.safe_email, age: Faker::Number.within(range: 18..90))
+end
